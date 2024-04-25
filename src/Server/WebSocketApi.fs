8593a264916b00833e1c263335fc1123bd5f7941
Joshua Villing <jsvilling@gmail.com>
2024-04-25 08:25:27 +0200

    wip

--- src/Server/WebSocketApi.fs
+++ src/Server/WebSocketApi.fs
@@ -1,18 +1,20 @@
 namespace Chess.Server
 
+open Chess
 open Giraffe
 open Elmish.Bridge
 
 open Chess.Api
 
 type ServerModel = {
-    State: int
+    GameState: GameState
 }
+
 
 [<RequireQualifiedAccess>]
 module ServerModel =
     let init = {
-        State = 0
+        GameState = GameState.init
     }
 
 [<RequireQualifiedAccess>]
@@ -21,8 +23,17 @@ module WebSocketApi =
     let private init _ _ =
         ServerModel.init, Elmish.Cmd.Empty
 
-    let private update _ _ model =
-        let newModel = { model with State = model.State + 1 }
+    let private update (clientDispatch: Elmish.Dispatch<ChessBoardMsg>) (msg: ServerMsg) (model: ServerModel) =
+
+        let newGameState =
+            match msg with
+            | ServerMsg.ResetGame -> GameState.init
+            | ServerMsg.MakeMove move -> GameState.update model.GameState move
+
+        clientDispatch (ChessBoardMsg.GotGameState (Ok newGameState))
+
+        let newModel = { model with GameState = newGameState }
+
         newModel, Elmish.Cmd.Empty
 
     let endpoint: HttpHandler =
